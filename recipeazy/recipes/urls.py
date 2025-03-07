48b849b55cb1187a70ea23350d13071bf3b6691e
beanz-on-toast <162033763+beanz-on-toast@users.noreply.github.com>
2025-03-07 07:22:06 +0000

    Adding header

--- recipeazy/recipes/urls.py
+++ recipeazy/recipes/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("recipe-list", views.recipe_list, name="recipe_list"),
     path("make-recipe", views.recipe_create,name="recipe-create"),
     path("recipe-list/<int:id>", views.recipe_detail,name="recipe-detail"),
-    path("recipe-list/", views.search_recipe, name="search-recipe"),
+    path("search-recipe/", views.search_recipe, name="search-recipe"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
